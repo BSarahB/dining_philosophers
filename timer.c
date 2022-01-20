af67a23bf88494ef978cc8a5090097406d5ceece
BSarahB <bslajta@gmail.com>
2022-01-21 00:31:08 +0100

    adding file

--- timer.c
+++ timer.c
@@ -7,14 +7,12 @@ unsigned int	curr_time(void)
 	unsigned int	sec_to_ms;
 	unsigned int	usec_to_ms;
 
-
 	curr_time = 0;
 	if (gettimeofday(&time, NULL) == -1)
 	{
 		perror("gettimeofday() failed");
 		return (0);
 	}
-	//convertit les secondes et microsecondes en milisecondes
 	sec_to_ms = time.tv_sec * 1000;
 	usec_to_ms = time.tv_usec / 1000;
 	curr_time = sec_to_ms + usec_to_ms;
@@ -23,15 +21,10 @@ unsigned int	curr_time(void)
 
 void	ft_call_usleep(unsigned int time_in_ms)
 {
-	//(void)time_in_ms;
 
 	unsigned int	start_time;
-	//conversion a l interieur :
-	//useconds_t		time_in_usec;
 	int				nb_intervals;
 	unsigned int	last_interval_ms;
-	//useconds_t		last_interval_usec;
-
 
 	start_time = curr_time();
 	nb_intervals = 1;
@@ -39,9 +32,6 @@ void	ft_call_usleep(unsigned int time_in_ms)
 
 	if (time_in_ms > 1000)
 	{
-		//on fait des ntervalles de 1 sec
-		//calculer le nb d itervalles necessaires: et la taille du dernier interval
-		//	->utilisons le modulo
 		nb_intervals = time_in_ms / 1000;
 		last_interval_ms = time_in_ms % 1000;
 
@@ -54,10 +44,8 @@ void	ft_call_usleep(unsigned int time_in_ms)
 		{
 			usleep(last_interval_ms * 1000);
 		}
-
 	}
 
-	else{
+	else
 		usleep(time_in_ms * 1000);
-	}
 }
